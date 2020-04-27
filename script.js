acb26741ce91aec26b334c79cad7dc03479062da
No, i want to stay anonymous <34982029+PeRSHH5Y98aF1JhpgDCL@users.noreply.github.com>
2020-04-28 05:22:46 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 dgi=(a)=>{return document.getElementById(a)}
 blah=(ele)=>{ele.innerHTML=ele.value}
-ireset=()=>{a.contentWindow.location.reload();}
+ireset=()=>{a.contentWindow.location.reload();setTimeout(()=>{Object.entries(window.iframeFunctions).forEach((x)=>{a.contentWindow.window[x[0]]=x[1]})},100);a.contentWindow.run=()=>{setTimeout(()=>{a.contentWindow.eval(window.parent.c.textContent)},100)}}
+dellist=(ele)=>{ele.parentElement.parentElement.removeChild(ele.parentElement)}
+delllist=(ele)=>{dellist(ele.parentElement)}
+del=(ele)=>{ele.parentElement.removeChild(ele)}
+var iframeFunctions={
+	makeElement:(x)=>{var y=document.createElement(x);a.contentDocument.body.appendChild(y);return y},
+	makeText:(x)=>{let y=document.createElement('p');y.innerHTML=x;a.contentDocument.body.appendChild(y);return y},
+	MER:(x)=>{var y=document.createElement(x);return y},
+	MTR:(x)=>{let y=document.createElement('p');y.innerHTML=x;return y},
+	sin:Math.sin
+}
 window.onload=()=>{
 window.a=dgi('a')
 window.b=dgi('b')
 window.c=dgi('c')
 window.d=dgi('d')
-window.run=function(){a.contentWindow.run()}
+window.run=function(){ireset();a.contentWindow.run()}
 new Sortable(b,{group:{name:'blob',pull:'clone',put:true}})
 new Sortable(c,{group:{name:'blob',pull:true,put:true}})
-new Sortable(d,{group:{name:'blob',pull:false,put:true}})
-setInterval(()=>{d.innerHTML=''},50)
-a.contentWindow.run=()=>{eval(window.parent.c.textContent)}
 setTimeout(()=>{
-[...document.getElementsByClassName('todragable')].forEach((x)=>{new Sortable(x,{group:"blob"})})
+[...document.querySelectorAll('.todragable')].forEach((x)=>{new Sortable(x,{group:"blob"})});
+[...document.getElementsByClassName('blue')].forEach((x)=>{new Sortable(x,{group:"blob",onAdd:(e)=>{
+	if(e.item.parentElement.childElementCount>1) {del(e.item)}
+}})})
 },50)}
