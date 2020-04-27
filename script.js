25db572b3eb52f81a6ebe7ab319128eaa23270d0
No, i want to stay anonymous <34982029+PeRSHH5Y98aF1JhpgDCL@users.noreply.github.com>
2020-04-28 05:33:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 dgi=(a)=>{return document.getElementById(a)}
 blah=(ele)=>{ele.innerHTML=ele.value}
 ireset=()=>{a.contentWindow.location.reload();setTimeout(()=>{Object.entries(window.iframeFunctions).forEach((x)=>{a.contentWindow.window[x[0]]=x[1]})},100);a.contentWindow.run=()=>{setTimeout(()=>{a.contentWindow.eval(window.parent.c.textContent)},100)}}
-dellist=(ele)=>{ele.parentElement.parentElement.removeChild(ele.parentElement)}
+del=(ele)=>{if(confirm("press cancel if you want to delete")){ele.parentElement.removeChild(ele)}}
+dellist=(ele)=>{del(ele.parenElement)}
 delllist=(ele)=>{dellist(ele.parentElement)}
-del=(ele)=>{ele.parentElement.removeChild(ele)}
+fdel=(ele)=>{ele.parentElement.removeChild(ele)}
 var iframeFunctions={
 	makeElement:(x)=>{var y=document.createElement(x);a.contentDocument.body.appendChild(y);return y},
 	makeText:(x)=>{let y=document.createElement('p');y.innerHTML=x;a.contentDocument.body.appendChild(y);return y},
@@ -22,6 +23,6 @@ new Sortable(c,{group:{name:'blob',pull:true,put:true}})
 setTimeout(()=>{
 [...document.querySelectorAll('.todragable')].forEach((x)=>{new Sortable(x,{group:"blob"})});
 [...document.getElementsByClassName('blue')].forEach((x)=>{new Sortable(x,{group:"blob",onAdd:(e)=>{
-	if(e.item.parentElement.childElementCount>1) {del(e.item)}
+	if(e.item.parentElement.childElementCount>1) {fdel(e.item)}
 }})})
 },50)}
